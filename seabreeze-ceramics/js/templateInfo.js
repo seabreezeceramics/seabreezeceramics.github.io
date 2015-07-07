c0ed2acf48c451fd146154fdc71f1b7e39e83aa5
Ben Oxley <ben.oxley@gmail.com>
2015-07-07 23:13:28 +0100

    Added JSON of info injection

---
+++ seabreeze-ceramics/js/templateInfo.js
@@ -0,0 +1,38 @@
+var templateInfo = {
+"dog":{
+	"title": "Dogs",
+	"prices": {
+		"largeFramed": '64.50',
+		"largeUnframed": '59.50',
+		"smallUnframed": '49.50',
+		"smallFramed": '59.50'
+	}
+},
+"cat":{
+	"title": "Cats",
+	"prices": {
+		"largeFramed": '64.50',
+		"largeUnframed": '59.50',
+		"smallUnframed": '49.50',
+		"smallFramed": '59.50'
+	}
+},
+"horse":{
+	"title": "Horses",
+	"prices": {
+		"largeFramed": '64.50',
+		"largeUnframed": '59.50',
+		"smallUnframed": '49.50',
+		"smallFramed": '59.50'
+	}
+},
+"baby":{
+	"title": "Babies",
+	"prices": {
+		"largeFramed": '64.50',
+		"largeUnframed": '59.50',
+		"smallUnframed": '49.50',
+		"smallFramed": '59.50'
+	}
+}
+};
