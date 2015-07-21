f4204563e1836aedf55a22cc7828531d3cfdb87c
Ben Oxley <ben.oxley@gmail.com>
2015-07-21 23:33:41 +0100

    Updated portfolio with images of new tiles

--- js/templateInfo.js
+++ js/templateInfo.js
@@ -1,38 +1,38 @@
 var templateInfo = {
 "dog":{
 	"title": "Dogs",
-	"prices": {
-		"largeFramed": '64.50',
-		"largeUnframed": '59.50',
-		"smallUnframed": '49.50',
-		"smallFramed": '59.50'
+	"sizes": {
+		"largeFramed": {"price" : '64.50'},
+		"largeUnframed": {"price" :'59.50'},
+		"smallUnframed": {"price" :'49.50'},
+		"smallFramed": {"price" :'59.50'}
 	}
 },
 "cat":{
 	"title": "Cats",
-	"prices": {
-		"largeFramed": '64.50',
-		"largeUnframed": '59.50',
-		"smallUnframed": '49.50',
-		"smallFramed": '59.50'
+	"sizes": {
+		"largeFramed": {"price" : '64.50'},
+		"largeUnframed": {"price" :'59.50'},
+		"smallUnframed": {"price" :'49.50'},
+		"smallFramed": {"price" :'59.50'}
 	}
 },
 "horse":{
 	"title": "Horses",
-	"prices": {
-		"largeFramed": '64.50',
-		"largeUnframed": '59.50',
-		"smallUnframed": '49.50',
-		"smallFramed": '59.50'
+	"sizes": {
+		"largeFramed": {"price" : '64.50'},
+		"largeUnframed": {"price" :'59.50'},
+		"smallUnframed": {"price" :'49.50'},
+		"smallFramed": {"price" :'59.50'}
 	}
 },
 "baby":{
 	"title": "Babies",
-	"prices": {
-		"largeFramed": '64.50',
-		"largeUnframed": '59.50',
-		"smallUnframed": '49.50',
-		"smallFramed": '59.50'
+	"sizes": {
+		"largeFramed": {"price" : '64.50'},
+		"largeUnframed": {"price" :'59.50'},
+		"smallUnframed": {"price" :'49.50'},
+		"smallFramed": {"price" :'59.50'}
 	}
 }
 };
