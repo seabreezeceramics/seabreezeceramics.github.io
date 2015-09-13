fd22dfd34d819184b75ab0d1c8cadc06cc113a0f
Ben Oxley <ben.oxley@gmail.com>
2015-09-13 13:42:17 +0100

    Updated template code

--- js/templateInfo.js
+++ js/templateInfo.js
@@ -2,10 +2,10 @@ var templateInfo = {
 "dog":{
 	"title": "Dogs",
 	"sizes": [
-		{"size":"largeFramed", "name":"Large Framed", "price" : '64.50',"image":"img/portfolio/largeframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"largeUnframed", "name":"Large Unframed", "price" :'59.50',"image":"img/portfolio/largeunframed50.jpg"},
-		{"size":"smallUnframed", "name":"Small Unframed", "price" :'49.50',"image":"img/portfolio/smallunframed50.jpg"},
-		{"size":"smallFramed", "name":"Small Framed", "price" :'59.50',"image":"img/portfolio/smallframed50.jpg"}
+		{"size":"largeFramed", "name":"Large Framed", "price" : '65.00',"image":"img/portfolio/largeframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"largeUnframed", "name":"Large Unframed", "price" :'60.00',"image":"img/portfolio/largeunframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"smallUnframed", "name":"Small Unframed", "price" :'50.00',"image":"img/portfolio/smallunframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"smallFramed", "name":"Small Framed", "price" :'60.00',"image":"img/portfolio/smallframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"}
 	]
 },
 "cat":{
