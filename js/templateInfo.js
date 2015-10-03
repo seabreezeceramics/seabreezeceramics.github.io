3962fee40b3963c8ce4bfc81bc71cf9fdd5d3a09
Ben Oxley <ben.oxley@gmail.com>
2015-10-03 16:08:26 +0100

    More updates to code and added pictures for babies and cats

--- js/templateInfo.js
+++ js/templateInfo.js
@@ -1,18 +1,20 @@
 var templateInfo = {
 "dog":{
 	"title": "Dogs",
+	"header" : "Our dog tiles are one of our most popular tiles! They make a great keepsake and look great on the wall. We know how much you dogs means to you and how much joy they bring. We cast their paws in ceramic and produce a beautiful impression of their paw.", 
 	"sizes": [
-		{"size":"largeFramed", "name":"Large Framed", "price" : '65.00',"image":"img/portfolio/largeframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"largeUnframed", "name":"Large Unframed", "price" :'60.00',"image":"img/portfolio/largeunframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"smallUnframed", "name":"Small Unframed", "price" :'50.00',"image":"img/portfolio/smallunframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"smallFramed", "name":"Small Framed", "price" :'60.00',"image":"img/portfolio/smallframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"}
+		{"size":"largeFramed", "name":"Large Framed Tile for Dogs", "price" : '65.00',"image":"img/portfolio/largeframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"largeUnframed", "name":"Large Unframed Tile for Dogs", "price" :'60.00',"image":"img/portfolio/largeunframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"smallUnframed", "name":"Small Unframed Tile for Dogs", "price" :'50.00',"image":"img/portfolio/smallunframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"smallFramed", "name":"Small Framed Tile for Dogs", "price" :'60.00',"image":"img/portfolio/smallframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"}
 	]
 },
 "cat":{
 	"title": "Cats",
+	"header" : "Our cat pawprints make great tiles (when you can catch them!) They make a great keepsake and look great on the wall. We know how much you cats means to you and how much joy they bring. We cast their paws in ceramic and produce a beautiful impression of their paw.", 
 	"sizes": [
-		{"size":"smallUnframed", "name":"Small Unframed", "price" :'49.50',"image":"img/portfolio/smallunframed50.jpg"},
-		{"size":"smallFramed", "name":"Small Framed", "price" :'59.50',"image":"img/portfolio/smallframed50.jpg"}
+		{"size":"smallUnframed", "name":"Unframed Tile for Cats", "price" :'50.00',"image":"img/portfolio/cat/cat_unframed.jpg","description":"Our cat pawprints make great tiles (when you can catch them!) This tile size is suitable for all cats (unless you have a lion!)"},
+		{"size":"smallFramed", "name":"Framed Tile for Cats", "price" :'60.00',"image":"img/portfolio/cat/cat_framed.jpg","description":"Our cat pawprints make great tiles (when you can catch them!) This tile size is suitable for all cats (unless you have a lion!)"}
 	]
 },
 "horse":{
@@ -26,11 +28,11 @@ var templateInfo = {
 },
 "baby":{
 	"title": "Babies",
-	"sizes": {
-		"largeFramed": {"price" : '64.50'},
-		"largeUnframed": {"price" :'59.50'},
-		"smallUnframed": {"price" :'49.50'},
-		"smallFramed": {"price" :'59.50'}
-	}
+	"sizes": [
+		{"size":"largeFramed", "name":"Large Framed Tile for Babies", "price" : '65.00',"image":"img/portfolio/baby/baby_large_framed.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"largeUnframed", "name":"Large Unframed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_large_framed.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"smallUnframed", "name":"Small Unframed Tile for Babies", "price" :'50.00',"image":"img/portfolio/baby/baby_small_unframed.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
+		{"size":"smallFramed", "name":"Small Framed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_small_unframed.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"}
+	]
 }
 };
