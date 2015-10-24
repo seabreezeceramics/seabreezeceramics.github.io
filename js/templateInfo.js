2c5dbe2e98a339137d07ef606de74bb86494604b
Ben Oxley <ben.oxley@gmail.com>
2015-10-24 02:22:08 +0100

    Updated website to be ready for updated cart

--- js/templateInfo.js
+++ js/templateInfo.js
@@ -13,26 +13,25 @@ var templateInfo = {
 	"title": "Cats",
 	"header" : "Our cat pawprints make great tiles (when you can catch them!) They make a great keepsake and look great on the wall. We know how much you cats means to you and how much joy they bring. We cast their paws in ceramic and produce a beautiful impression of their paw.", 
 	"sizes": [
-		{"size":"smallUnframed", "name":"Unframed Tile for Cats", "price" :'50.00',"image":"img/portfolio/cat/cat_unframed.jpg","description":"Our cat pawprints make great tiles (when you can catch them!) This tile size is suitable for all cats (unless you have a lion!)"},
-		{"size":"smallFramed", "name":"Framed Tile for Cats", "price" :'60.00',"image":"img/portfolio/cat/cat_framed.jpg","description":"Our cat pawprints make great tiles (when you can catch them!) This tile size is suitable for all cats (unless you have a lion!)"}
+		{"size":"smallUnframed", "name":"Unframed Tile for Cats", "price" :'50.00',"image":"img/portfolio/cat/cat_unframed.jpg","description":"Cat pawprints make great tiles (when you can catch them!) This tile size is suitable for all cats (unless you have a lion!)"},
+		{"size":"smallFramed", "name":"Framed Tile for Cats", "price" :'60.00',"image":"img/portfolio/cat/cat_framed.jpg","description":"Cat pawprints make great tiles (when you can catch them!) This tile size is suitable for all cats (unless you have a lion!)"}
 	]
 },
 "horse":{
 	"title": "Horses",
-	"sizes": {
-		"largeFramed": {"price" : '64.50'},
-		"largeUnframed": {"price" :'59.50'},
-		"smallUnframed": {"price" :'49.50'},
-		"smallFramed": {"price" :'59.50'}
-	}
+	"header" : "Horses are a huge part of many people's lives including ours! Despite their size, we can still make their hoofprints into our beautiful handmade tiles.", 
+	"sizes": [
+		{"size":"smallUnframed","name":"Unframed Tile for Horses","price" : '60.00',"image":"img/portfolio/horse/horse_framed.jpg","description":"This tile size is suitable for all horses."},
+		{"size":"smallFramed","name":"Framed Tile for Horses","price" : '65.00',"image":"img/portfolio/horse/horse_framed2.jpg","description":"This tile size is suitable for all horses."}
+	]
 },
 "baby":{
 	"title": "Babies",
 	"sizes": [
-		{"size":"largeFramed", "name":"Large Framed Tile for Babies", "price" : '65.00',"image":"img/portfolio/baby/baby_large_framed.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"largeUnframed", "name":"Large Unframed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_large_framed.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"smallUnframed", "name":"Small Unframed Tile for Babies", "price" :'50.00',"image":"img/portfolio/baby/baby_small_unframed.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"smallFramed", "name":"Small Framed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_small_unframed.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"}
+		{"size":"largeFramed", "name":"Large Framed Tile for Babies", "price" : '65.00',"image":"img/portfolio/baby/baby_large_framed.jpg", "description":"A large tile for two hands, two feet or one of each."},
+		{"size":"largeUnframed", "name":"Large Unframed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_large_framed.jpg", "description":"A large tile for two hands, two feet or one of each."},
+		{"size":"smallUnframed", "name":"Small Unframed Tile for Babies", "price" :'50.00',"image":"img/portfolio/baby/baby_small_unframed.jpg", "description":"A single hand or footprint on a tile."},
+		{"size":"smallFramed", "name":"Small Framed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_small_unframed.jpg", "description":"A single hand or footprint on a tile."}
 	]
 }
 };
