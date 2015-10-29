61d57adddbee286aec05e88cdcf95704996641fb
Ben Oxley <ben.oxley@gmail.com>
2015-10-29 22:24:54 +0000

    Updated website text and testimonial

--- js/templateInfo.js
+++ js/templateInfo.js
@@ -1,17 +1,17 @@
 var templateInfo = {
 "dog":{
 	"title": "Dogs",
-	"header" : "Our dog tiles are one of our most popular tiles! They make a great keepsake and look great on the wall. We know how much you dogs means to you and how much joy they bring. We cast their paws in ceramic and produce a beautiful impression of their paw.", 
+	"header" : "Our dog tiles are one of our most popular tiles! They make a great keepsake and look wonderful displayed on the wall. We know how much your dogs mean to you and how much joy they bring.", 
 	"sizes": [
 		{"size":"largeFramed", "name":"Large Framed Tile for Dogs", "price" : '65.00',"image":"img/portfolio/largeframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
 		{"size":"largeUnframed", "name":"Large Unframed Tile for Dogs", "price" :'60.00',"image":"img/portfolio/largeunframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"smallUnframed", "name":"Small Unframed Tile for Dogs", "price" :'50.00',"image":"img/portfolio/smallunframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"},
-		{"size":"smallFramed", "name":"Small Framed Tile for Dogs", "price" :'60.00',"image":"img/portfolio/smallframed50.jpg", "description":"For bigger dogs such as German Shepherds, Labradors, Bull Terriers and Border Collies"}
+		{"size":"smallUnframed", "name":"Small Unframed Tile for Dogs", "price" :'50.00',"image":"img/portfolio/smallunframed50.jpg", "description":"For smaller dogs such as Chihuahas, Jack Russel Terriers, Miniature Poodles and small puppies"},
+		{"size":"smallFramed", "name":"Small Framed Tile for Dogs", "price" :'60.00',"image":"img/portfolio/smallframed50.jpg", "description":"For smaller dogs such as Chihuahas, Jack Russel Terriers, Miniature Poodles and small puppies"}
 	]
 },
 "cat":{
 	"title": "Cats",
-	"header" : "Our cat pawprints make great tiles (when you can catch them!) They make a great keepsake and look great on the wall. We know how much you cats means to you and how much joy they bring. We cast their paws in ceramic and produce a beautiful impression of their paw.", 
+	"header" : "Our cat pawprints make great tiles (when you can catch them!) We cast their paws in ceramic and produce a beautiful keepsake for you to treasure.", 
 	"sizes": [
 		{"size":"smallUnframed", "name":"Unframed Tile for Cats", "price" :'50.00',"image":"img/portfolio/cat/cat_unframed.jpg","description":"Cat pawprints make great tiles (when you can catch them!) This tile size is suitable for all cats (unless you have a lion!)"},
 		{"size":"smallFramed", "name":"Framed Tile for Cats", "price" :'60.00',"image":"img/portfolio/cat/cat_framed.jpg","description":"Cat pawprints make great tiles (when you can catch them!) This tile size is suitable for all cats (unless you have a lion!)"}
@@ -21,18 +21,18 @@ var templateInfo = {
 	"title": "Horses",
 	"header" : "Horses are a huge part of many people's lives including ours! Despite their size, we can still make their hoofprints into our beautiful handmade tiles.", 
 	"sizes": [
-		{"size":"smallUnframed","name":"Unframed Tile for Horses","price" : '60.00',"image":"img/portfolio/horse/horse_unframed.jpg","description":"Some have massive hooves, some have tiny but we do a one size fits all for everything from Falabellas to Clydesdales."},
-		{"size":"smallFramed","name":"Framed Tile for Horses","price" : '65.00',"image":"img/portfolio/horse/horse_framed.jpg","description":"Some have massive hooves, some have tiny but we do a one size fits all for everything from Falabellas to Clydesdales."}
+		{"size":"smallUnframed","name":"Unframed Tile for Horses","price" : '60.00',"image":"img/portfolio/horse/horse_unframed.jpg","description":"Some have massive hooves, some have tiny ones but we do a one size fits all for everything from Falabellas to Clydesdales. If your horse has particularly massive hooves, please let us know in the comments when you checkout your order as we will need to send a larger piece of dough."},
+		{"size":"smallFramed","name":"Framed Tile for Horses","price" : '65.00',"image":"img/portfolio/horse/horse_framed.jpg","description":"Some have massive hooves, some have tiny ones but we do a one size fits all for everything from Falabellas to Clydesdales. If your horse has particularly massive hooves, please let us know in the comments when you checkout your order as we will need to send a larger piece of dough."}
 	]
 },
 "baby":{
 	"title": "Babies",
-	"header" : "We make tiles of babies hand and footprints for new parents and for the whole family. They grow up so fast and our tiles provide an amazing keepsake of the special day to trasure forever.",
+	"header" : "We make tiles of babies hand and footprints for new parents and for the whole family. They grow up so fast and our tiles provide an amazing keepsake of the special day to treasure forever. It's a wonderful way to remember how tiny they were!",
 	"sizes": [
-		{"size":"largeFramed", "name":"Large Framed Tile for Babies", "price" : '65.00',"image":"img/portfolio/baby/baby_large_framed.jpg", "description":"A large tile for two hands, two feet or one of each. This makes an amazing gift, or keepsake. The high quality of the case shows up every detail of their hands and feet."},
-		{"size":"largeUnframed", "name":"Large Unframed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_large_unframed.jpg", "description":"A large tile for two hands, two feet or one of each. This makes an amazing gift, or keepsake. The high quality of the case shows up every detail of their hands and feet."},
-		{"size":"smallUnframed", "name":"Small Unframed Tile for Babies", "price" :'50.00',"image":"img/portfolio/baby/baby_small_unframed.jpg", "description":"A single hand or footprint on a tile, makes an amazing gift, or keepsake. The high quality of the case shows up every detail of their hands and feet."},
-		{"size":"smallFramed", "name":"Small Framed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_small_framed.jpg", "description":"A single hand or footprint, makes an amazing gift, or keepsake. The high quality of the case shows up every detail of their hands and feet."}
+		{"size":"largeFramed", "name":"Large Framed Tile for Babies", "price" : '65.00',"image":"img/portfolio/baby/baby_large_framed.jpg", "description":"A large tile for two hands, two feet or one of each. This makes an amazing gift, or keepsake. The high quality of the cast shows up every detail of their hands and feet."},
+		{"size":"largeUnframed", "name":"Large Unframed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_large_unframed.jpg", "description":"A large tile for two hands, two feet or one of each. This makes an amazing gift, or keepsake. The high quality of the cast shows up every detail of their hands and feet."},
+		{"size":"smallUnframed", "name":"Small Unframed Tile for Babies", "price" :'50.00',"image":"img/portfolio/baby/baby_small_unframed.jpg", "description":"A single hand or footprint on a tile makes an fantastic gift, or keepsake. The high quality of the cast shows up every detail of their hands and feet. This size only has room for impressions of babies of five months old or younger."},
+		{"size":"smallFramed", "name":"Small Framed Tile for Babies", "price" :'60.00',"image":"img/portfolio/baby/baby_small_framed.jpg", "description":"A single hand or footprint on a tile makes an fantastic gift, or keepsake. The high quality of the cast shows up every detail of their hands and feet. This size only has room for impressions of babies of five months old or younger."}
 	]
 }
 };
